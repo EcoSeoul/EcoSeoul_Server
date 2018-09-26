b94e80e7f6f0b36425e2b65750208d5d58788b44
HyunJin <hj97718@naver.com>
2018-09-26 14:50:01 +0900

    update calc module

--- EcoSeoul/module/calc.js
+++ EcoSeoul/module/calc.js
@@ -6,18 +6,18 @@ module.exports = {							// 두 개의 메소드 module화
         
         if (past > present) {   //절약
             result = {
-                up_down : 0,
+                up_down : 1,
                 percentage : Math.round(present / past * 100)
             }
         } else if (past < present) {
             result = {     //과소비
-                up_down : 1,
+                up_down : 2,
                 percentage : Math.round(past / present * 100)
                 
             }
         } else {
             result = {     //같음
-                up_down : 2,
+                up_down : 0,
                 percentage : 0    
             }
         }
